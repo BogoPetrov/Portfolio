9ae46e3614b6761225386b10d26b207b12fc9ba9
Bogomil Petorv <bogopetrov07@gmail.com>
2025-02-05 16:00:48 +0200

    Update to date 05.02.2025

---
+++ Concurrent programming/22.01.2025/Execise_Task/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace Execise_Task
+{
+    using System;
+    using System.Diagnostics;
+    using System.Threading.Tasks;
+
+    public class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Random random = new();
+
+            int pizzaTime = random.Next(2000, 5000);
+            int pastaTime = random.Next(2000, 5000);
+            int dessertTime = random.Next(4000, 7000);
+
+            Task pizzaTask = PreparePizza(pizzaTime);
+            Task pastaTask = PreparePasta(pastaTime);
+            Task dessertTask = PrepareDessert(dessertTime);
+
+            Stopwatch stopwatch = Stopwatch.StartNew();
+
+            pizzaTask.Wait();
+            await pastaTask;
+            await dessertTask;
+
+            stopwatch.Stop();
+
+            Console.WriteLine("\nAll dishes are ready!");
+            Console.WriteLine($"Total preparation time: {stopwatch.ElapsedMilliseconds / 1000.0} seconds.");
+            Console.ReadKey(true);
+        }
+
+        public static Task PreparePizza(int delay)
+        {
+            Console.WriteLine("Pizza preparation starts...");
+            Task.Delay(delay).Wait();
+            Console.WriteLine($"Pizza is ready after {delay / 1000.0} seconds.");
+            return Task.CompletedTask;
+        }
+
+        public static async Task PreparePasta(int delay)
+        {
+            Console.WriteLine("Pasta preparation starts...");
+            await Task.Delay(delay);
+            Console.WriteLine($"Pasta is ready after {delay / 1000.0} seconds.");
+        }
+
+        public static async Task PrepareDessert(int delay)
+        {
+            Console.WriteLine("Dessert preparation starts...");
+            await Task.Delay(delay);
+            Console.WriteLine($"Dessert is ready after {delay / 1000.0} seconds.");
+        }
+    }
+}
---
+++ Concurrent programming/29.01.2025/Task_Summary/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace Task_Summary
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            //CallMethod();
+            Task t1 = Task.Run(() =>
+            {
+                PrintInfo();
+            });
+
+            t1.Wait();
+
+            Console.WriteLine("Main Thread Completed");
+
+            Console.ReadKey(true);
+        }
+
+        public static async void PrintInfo()
+        {
+            for (int i = 0; i <= 4; i++)
+            {
+                Console.WriteLine($"{Task.CurrentId} value: {i}");
+                await Task.Delay(1000);
+            }
+        }
+
+        public static async void CallMethod()
+        {
+            Task<int> task = Method1();
+            Method2();
+            int count = await task;
+            Method3(count);
+        }
+
+        public static async Task<int> Method1()
+        {
+            int count = 0;
+            await Task.Run(() =>
+            {
+                for (int i = 0; i < 100; i++)
+                {
+                    Console.WriteLine("Method 1");
+                    count++;
+                }
+            });
+            return count;
+        }
+
+        public static void Method2()
+        {
+            for (int i = 0; i < 25; i++)
+            {
+                Console.WriteLine("Method 2");
+            }
+        }
+
+        public static void Method3(int count)
+        {
+            Console.WriteLine($"Total count is {count}");
+        }
+    }
+}
